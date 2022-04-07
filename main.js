3efa9e47ee9e827fed8d88cd4598a6001102da78
Thassius <thassius.carrion@hotmail.com>
2022-04-07 20:24:13 -0300

    project frontend

---
+++ main.js
@@ -0,0 +1,75 @@
+
+function fazPost(url, body) {
+    console.log(body)
+    let request = new XMLHttpRequest()
+    request.open("POST", url, true)
+    request.setRequestHeader("Content-type", "application/json")
+    request.send(JSON.stringify(body))
+    request.onload = function() {
+        console.log(this.responseText)
+    }
+    return request.responseText
+}
+
+function cadastrarProduto(){
+    event.preventDefault()
+    let url = "http://localhost:8000/home/products"
+    let name = document.getElementById("name").value
+    let price =  document.getElementById("price").value
+    let details = document.getElementById("details").value
+    console.log(name)
+    console.log(price)
+
+    body = {
+        "name": name,
+        "price": price,
+        "details": details
+    }
+
+    fazPost(url, body)
+
+    fetch('http://localhost:8000/home/products')
+        .then(response => response.json())
+        .then(data => { 
+            deletarDados(data)
+    })
+
+    fetch('http://localhost:8000/home/products')
+        .then(response => response.json())
+        .then(data => { 
+            popularDados(data)
+    })
+}
+
+fetch('http://localhost:8000/home/products')
+.then(response => response.json())
+.then(data => { 
+    popularDados(data)
+})
+
+function deletarDados(jsonObject) {
+    for (let i = 0; i < jsonObject.length; i++) {
+        document.getElementById("productTable").deleteRow(i);
+    }
+}
+
+
+function popularDados(jsonObject) {
+    let myTable = document.getElementById("productTable");
+
+    for (let i = 0; i < jsonObject.length; i++) {
+        let myRow = myTable.insertRow(-1);
+
+        let cell = myRow.insertCell(0);
+        cell.appendChild(document.createTextNode(jsonObject[i].id));
+
+        cell = myRow.insertCell(1);
+        cell.appendChild(document.createTextNode(jsonObject[i].name));
+
+        cell = myRow.insertCell(2);
+        cell.appendChild(document.createTextNode(jsonObject[i].price));
+
+        cell = myRow.insertCell(3);
+        cell.appendChild(document.createTextNode(jsonObject[i].details));
+    }
+}
